816824dad8a6828b45e34074ff2165aed91866df
athul-jose00 <148617363+athul-jose00@users.noreply.github.com>
2026-05-29 23:07:05 +0530

    fix: replace bcrypt with bcryptjs for consistency in dependencies

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import prisma from "../config/prisma.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import { sendResetEmail } from "../utils/email.js";
